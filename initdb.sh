38577ceb7b99644913a54cb38226e115d0a42c0b
martind <martin@dekstop.de>
2018-03-28 12:44:45 +0000

    load states/provinces boundaries

--- initdb.sh
+++ initdb.sh
@@ -21,16 +21,30 @@ etldir=${datadir}/init
 
 mkdir -p $etldir 2>&1
 
-neshapefile=~/oiidg/natural-earth/ne_10m_admin_0_countries.shp
-nesqlfile=${etldir}/ne_10m_admin_0_countries.sql
+##
+## Shapefiles
+##
+
+function load_shapefile() {
+    tablename=$1
+    shapefile=$2
+    sqlfile=$3
+
+    time $PSQL -c "DROP TABLE IF EXISTS ${tablename} CASCADE" || return 1
+    shp2pgsql -c -I ${shapefile} $tablename > ${sqlfile} || return 1
+    time $PSQL < ${sqlfile} || return 1
+}
+
+load_shapefile countries \
+    ~/oiidg/natural-earth/ne_10m_admin_0_countries/ne_10m_admin_0_countries.shp \
+    ${etldir}/ne_10m_admin_0_countries.sql || exit 1
+
+load_shapefile provinces \
+    ~/oiidg/natural-earth/ne_10m_admin_1_states_provinces/ne_10m_admin_1_states_provinces.shp \
+    ${etldir}/ne_10m_admin_1_states_provinces.sql || exit 1
 
 ##
 ## DB schema
 ##
-
-time $PSQL -c "DROP TABLE IF EXISTS countries CASCADE" || exit 1
-shp2pgsql -c -I ${neshapefile} countries > ${nesqlfile} || exit 1
-time $PSQL < ${nesqlfile} || exit 1
-
 # time $PSQL < ${srcdir}/globalschema.sql || exit 1
 
