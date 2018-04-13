771216810debfb6ae9cb2bc8b1502104e0ff2f1a
martind <martin@dekstop.de>
2018-04-13 10:45:21 +0000

    wiki stats table

--- allwikis.sql
+++ allwikis.sql
@@ -26,8 +26,9 @@ CREATE MATERIALIZED VIEW allwikis.country_editorloc AS
     GROUP BY ae.wiki, ac.gid, ae.iso2;
 
 CREATE MATERIALIZED VIEW allwikis.wiki_stats AS
-    SELECT t1.wiki, size, num_pages, num_articles, num_gt_articles,
-        num_gt_articles * 1.0 / num_articles as share_gt_articles
+    SELECT t1.wiki, size, num_pages, num_articles, 
+        num_gt_articles, num_gt_articles * 1.0 / num_articles as share_gt_articles,
+        num_controv_articles, num_controv_articles * 1.0 / num_articles as share_controv_articles
     FROM (-- namespace sizes
         SELECT n.nspname::wiki as wiki, pg_catalog.pg_size_pretty(sum(pg_total_relation_size(pg_class.oid::regclass))) as size 
         FROM pg_class 
@@ -40,8 +41,8 @@ CREATE MATERIALIZED VIEW allwikis.wiki_stats AS
         GROUP BY wiki
     ) t1
     JOIN (-- page/article stats
-        SELECT wiki, count(distinct(page)) num_pages,
-            count(distinct(page)) FILTER (WHERE ns=0) num_articles
+        SELECT wiki, count(distinct page) num_pages,
+            count(distinct page) FILTER (WHERE ns=0) num_articles
         FROM allwikis.page_stats
         GROUP BY wiki
     ) t2 ON (t1.wiki=t2.wiki)
@@ -50,4 +51,10 @@ CREATE MATERIALIZED VIEW allwikis.wiki_stats AS
         FROM allwikis.article_geotag_primary
         GROUP BY wiki
     ) t3 ON (t1.wiki=t3.wiki)
+    LEFT OUTER JOIN (-- controversy stats
+        SELECT wiki, count(distinct page) num_controv_articles
+        FROM allwikis.page_controversy
+        WHERE ns=0 AND controversy_score>=1000
+        GROUP BY wiki
+    ) t4 ON (t1.wiki=t4.wiki)
     ORDER BY t1.wiki;
