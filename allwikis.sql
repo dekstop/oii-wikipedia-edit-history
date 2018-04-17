4d6f63a25a8bfbe9d65409189ea560a3cca2cc78
martind <martin@dekstop.de>
2018-04-17 13:50:02 +0000

    utility stats view

--- allwikis.sql
+++ allwikis.sql
@@ -1,6 +1,18 @@
 DROP SCHEMA IF EXISTS allwikis CASCADE;
 CREATE SCHEMA allwikis;
 
+-- Utility: what tables are available per wiki, and how big are thy in total?
+CREATE OR REPLACE VIEW allwikis.view_wiki_tablesizes AS
+    SELECT pg_class.relname, count(distinct nspname) as num_wikis, 
+        sum(pg_class.reltuples) as reltuples, 
+        pg_catalog.pg_size_pretty(sum(pg_total_relation_size(pg_class.oid::regclass))) as size 
+    FROM pg_class 
+    JOIN pg_catalog.pg_namespace n ON n.oid=pg_class.relnamespace 
+    WHERE pg_class.relkind IN ('r'::"char", 'm'::"char")
+    AND n.nspname IN (SELECT get_wiki_namespaces())
+    GROUP BY pg_class.relname;
+
+-- Views across wiki namespaces
 SELECT create_wiki_enum_type();
 SELECT create_multiwiki_table_view('allwikis', 'view_article_revisions');
 SELECT create_multiwiki_table_view('allwikis', 'page_stats');
@@ -9,6 +21,10 @@ SELECT create_multiwiki_table_view('allwikis', 'article_geotags');
 SELECT create_multiwiki_table_view('allwikis', 'article_geotag_primary');
 SELECT create_multiwiki_table_view('allwikis', 'article_country');
 SELECT create_multiwiki_table_view('allwikis', 'article_province');
+
+--
+-- Derivative tables
+--
 
 CREATE MATERIALIZED VIEW allwikis.article_editorloc AS
     SELECT wiki, page, iso2, count(*) num_revisions 
@@ -24,6 +40,25 @@ CREATE MATERIALIZED VIEW allwikis.country_editorloc AS
     FROM allwikis.article_editorloc ae
     JOIN allwikis.article_country ac ON (ae.wiki=ac.wiki AND ae.page=ac.page)
     GROUP BY ae.wiki, ac.gid, ae.iso2;
+
+-- A utility table to track annual page activity. For every page, a row for 
+-- every year of its existence, with a flag for the initial year of page creation.
+CREATE MATERIALIZED VIEW allwikis.page_years AS
+    WITH all_years AS (
+        SELECT first_year + generate_series(0,last_year-first_year) as year
+        FROM (
+            SELECT 
+                date_part('year', min(created_at))::int first_year, 
+                date_part('year', max(created_at))::int last_year
+            FROM allwikis.page_stats
+        )t1)
+    SELECT year, wiki, page, (date_part('year', created_at)::int=year) as is_first_year
+    FROM allwikis.page_stats
+    JOIN all_years ON date_part('year', created_at)::int<=year;
+
+--
+-- Stats tables
+--
 
 CREATE MATERIALIZED VIEW allwikis.wiki_stats AS
     SELECT t1.wiki, size, num_pages, num_articles, 
@@ -59,17 +94,3 @@ CREATE MATERIALIZED VIEW allwikis.wiki_stats AS
     ) t4 ON (t1.wiki=t4.wiki)
     ORDER BY t1.wiki;
 
--- A helper join table to track annual page activity. For every page, a row for 
--- every year of its existence, with a flag for the initial year of page creation.
-CREATE MATERIALIZED VIEW allwikis.page_years AS
-    WITH all_years AS (
-        SELECT first_year + generate_series(0,last_year-first_year) as year
-        FROM (
-            SELECT 
-                date_part('year', min(created_at))::int first_year, 
-                date_part('year', max(created_at))::int last_year
-            FROM allwikis.page_stats
-        )t1)
-    SELECT year, page, (date_part('year', created_at)::int=year) as is_first_year
-    FROM allwikis.page_stats
-    JOIN all_years ON date_part('year', created_at)::int<=year;
